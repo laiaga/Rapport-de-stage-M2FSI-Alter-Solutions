002ffff7fc36190e4a07a1317f85a3969b4b117c
Alexandre Leonardi <leonardialexandre@free.fr>
2017-07-10 10:09:41 +0200

    je ne sias plus ce que j'ai fait :(

--- cpistuff/auto/cpi.el
+++ cpistuff/auto/cpi.el
@@ -3,12 +3,12 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("fontenc" "T1") ("inputenc" "utf8x") ("babel" "frenchb")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "color"
@@ -21,7 +21,7 @@
     "hyperref"
     "amssymb"
     "enumitem"
-    "pgfplots")
+    "pgf-pie")
    (TeX-add-symbols
     '("slice" 4)
     '("callout" 1)
